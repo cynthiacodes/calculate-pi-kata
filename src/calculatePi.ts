36f8a81b5192416353192e2d26feb73e7c1f75ee
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-07 10:18:10 +0100

    added calculatePi function implementation

---
+++ src/calculatePi.ts
@@ -0,0 +1,38 @@
+// estimate(4) = 4 * (+ 1/1 – 1/3 + 1/5 – 1/7)
+/**
+ function calculatePi:
+ input: number
+ output: number
+
+ declare oddNumArray = []
+ 
+for(i= 1; i < inputNum *2 ; i += 2) do:
+    oddNumArray.push(i);
+EndFor
+
+let parameter: number;
+for amount of elements = oddNumArray.length do:
+
+result = inputNum * ()
+
+ 
+*/
+
+function calculatePi(inputNum: number): number {
+  let oddNumArray: number[] = [];
+
+  for (let oddNum = 1; oddNum < inputNum * 2; oddNum += 2) {
+    oddNumArray.push(oddNum);
+  }
+
+  let parameter:number = 0;
+  for (let i = 0; i < oddNumArray.length; i++) {
+    if (i % 2 === 0) {
+      parameter += 1 / oddNumArray[i];
+    } else {
+      parameter -= 1 / oddNumArray[i];
+    }
+  }
+  
+  return inputNum * parameter ;
+}
